6a6317ba23a509fa439c075fe404df486973db03
JCTA0125 <jcta0125@naver.com>
2024-06-25 15:14:38 +0900

    lecture11-1 : tableView

---
+++ Movie/MovieController.swift
@@ -0,0 +1,103 @@
+//
+//  MovieController.swift
+//  Movie
+//
+//  Created by mac12 on 5/17/24.
+//
+
+import UIKit
+
+class MovieController: UITableViewController {
+    let movies = ["Chasing Amy", "Mallrats", "Dogma",
+                  "Clerks", "Jay & Silent Bob Strike Back", "Red State",
+                  "Cop Out", "Jersey Girl"]
+    var movieImages: [UIImage] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var i = 0
+        for _ in movies {
+            let image = UIImage(named: "\(i).jpg")
+            movieImages.append(image!)
+            i = i + 1
+        }
+                        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return movies.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = movies[indexPath.row]
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        let destination = segue.destination as? DetailController
+        let index = tableView.indexPathForSelectedRow?.row
+        if let destination = destination, let index = index {
+            destination.image = self.movieImages[index]
+        }
+    }
+    
+
+}
---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by mac12 on 5/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
