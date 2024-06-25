63b537cd72f9417e9f1e60a122cbb294f7f6f0d8
JCTA0125 <jcta0125@naver.com>
2024-06-25 14:39:31 +0900

    week8 : age calculator

---
+++ Homework7/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Homework7
+//
+//  Created by mac12 on 4/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var ageTextField: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func convertButton(_ sender: Any) {
+        guard let name = nameTextField.text, !name.isEmpty else {
+            resultLabel.text = "name을 입력하세요"
+            return
+        }
+        
+        guard let ageStr = ageTextField.text, !ageStr.isEmpty, let age = Int(ageStr) else {
+            resultLabel.text = "age를 입력하세요"
+            return
+        }
+        
+        resultLabel.text = determineDing(name: name, age: age)
+        
+        nameTextField.resignFirstResponder()
+        ageTextField.resignFirstResponder()
+    }
+    func determineDing(name: String, age: Int) -> String {
+        switch age {
+        case 8..<14:
+            return "\(name)님은 초딩"
+        case 14..<17:
+            return "\(name)님은 중딩"
+        case 17..<20:
+            return "\(name)님은 고딩"
+        default:
+            return "\(name)님은 기타"
+        }
+    }
+    @IBAction func comeback(segue: UIStoryboardSegue){
+        
+    }
+}
